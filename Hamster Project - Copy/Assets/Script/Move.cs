9f1da4e3bd6fc18098a37ebfe08cfec02077abad
MunyinSam <kelvinsam233@gmail.com>
2023-12-09 18:09:44 +0700

    Delete

--- Hamster Project - Copy/Assets/Script/Move.cs
+++ Hamster Project - Copy/Assets/Script/Move.cs
@@ -3,16 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Move : MonoBehaviour
+
+   
 {
+    private Rigidbody2D rb;
+    private bool moveLeft;
+    private bool moveRight;
+    private float horizontalMove;
+    public float speed = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
+        moveLeft = false;
+        moveRight = false;
     }
 
-    // Update is called once per frame
+    public void PointerDownLeft()
+    {
+        moveLeft = true;
+    }
+
+    public void PointerUpLeft()
+    {
+        moveRight = false;
+    }
+
+    public void PointerDownRight()
+    {
+        moveRight = true;
+    }
+
+    public void PointerUpRight()
+    {
+        moveLeft = false;
+    }
+
     void Update()
     {
-        
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        if (moveLeft)
+        {
+            horizontalMove = -speed;
+        }
+        else if (moveRight)
+        {
+            horizontalMove = speed;
+        }
     }
 }
